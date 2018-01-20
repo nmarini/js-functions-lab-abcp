7d55f2fe94c83d7d5c1972cca586a498eee4c05c
Nicolas Marini <nmarini44@gmail.com>
2018-01-20 15:15:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // write your code below!
 function happyHolidays() {return "Happy holidays!"} 
 
-function happyHolidaysTo() {return "Happy holidays, you!"}
+function happyHolidayTo() {return "Happy holidays, you!"}
